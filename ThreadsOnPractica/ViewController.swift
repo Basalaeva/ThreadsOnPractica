ce88665a725b4da3077cb13227358a52bcab8dd1
Basalaeva <basalaeva.ecaterina@yandex.ru>
2019-04-27 10:47:01 +0700

    Initial Commit

---
+++ ThreadsOnPractica/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThreadsOnPractica
+//
+//  Created by Kate on 27/04/2019.
+//  Copyright © 2019 Kate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
